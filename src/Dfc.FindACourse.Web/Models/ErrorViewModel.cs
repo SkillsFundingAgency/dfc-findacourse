8689d6eff553988ce5d76b670934470749955cbf
Mahmood Akhtar <mahmood233@hotmail.com>
2018-08-06 15:25:54 +0100

    First commit. A work in prgress.

---
+++ src/Dfc.FindACourse.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dfc.FindACourse.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
