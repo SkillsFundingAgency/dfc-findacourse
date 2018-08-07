9893348e178f7baa9f30d7eb47a0f9ed4906f367
Mahmood Akhtar <mahmood233@hotmail.com>
2018-08-07 16:19:26 +0100

    Removing unnecessary controller and view files. Changed the default MapRoute.

--- src/Dfc.FindACourse.Web/Startup.cs
+++ src/Dfc.FindACourse.Web/Startup.cs
@@ -56,7 +56,7 @@ namespace Dfc.FindACourse.Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=CourseDirectory}/{action=Index}/{id?}");
             });
         }
     }
