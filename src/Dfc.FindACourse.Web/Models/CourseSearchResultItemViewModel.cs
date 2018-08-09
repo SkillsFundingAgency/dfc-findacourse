232ad09825c062e7ad5f3b2ea98d4b93f99bf248
Mahmood Akhtar <mahmood233@hotmail.com>
2018-08-09 16:46:58 +0100

    DFC-4031 Workout the ViewModel for the "Course Search Results" page

---
+++ src/Dfc.FindACourse.Web/Models/CourseSearchResultItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfc.FindACourse.Web.Models
+{
+    public class CourseSearchResultItemViewModel
+    {
+        public string CourseTitle { get; set; }
+        public string QualificationLevel { get; set; }
+        public string StudyMode { get; set; }
+        public string AttendanceMode { get; set; }
+        public string AttendencePattern { get; set; }
+        public string ProviderName { get; set; }
+        public string Location { get; set; }
+        public string Distance { get; set; }
+        public string StartDate { get; set; }
+        public string Duration { get; set; }
+    }
+}
---
+++ src/Dfc.FindACourse.Web/Models/CourseSearchResultViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfc.FindACourse.Web.Models
+{
+    public class CourseSearchResultViewModel
+    {
+        public string SortyBy { get; set; }
+        public int StartNo { get; set; }
+        public int EndNo { get; set; }
+        public int NoOfRecords { get; set; }
+        public int PageNo { get; set; }
+        public int NoOfPages { get; set; }
+        public List<CourseSearchResultItemViewModel> Items { get; set; }
+    }
+}
