b6088d9fafd1cea8bd8534cdc6f01cacefffc14c
Richard Lee <ric@dicci.co.uk>
2018-08-14 15:32:01 +0100

    Initial ARM template

---
+++ azure/ConvertTo-VSTSVariables.ps1
@@ -0,0 +1,14 @@
+param (
+    [Parameter(Mandatory=$true)][string]$ARMOutput
+)
+
+# Output from ARM template is a JSON document
+$jsonvars = $ARMOutput | convertfrom-json
+
+# VSTS variables to be extracted from JSON
+$storageAccountName = $jsonvars.storageAccountName.value
+$storageConnectionString = $json.storageConnectionString.value
+
+# Write these out as VSTS variables
+Write-Host "##vso[task.setvariable variable=storageAccountName]$storageAccountName"
+Write-Host "##vso[task.setvariable variable=storageConnectionString]$storageConnectionString"
