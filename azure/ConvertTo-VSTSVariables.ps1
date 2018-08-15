42147d02aaefd6f5116dcb1080eb8554acccdc7d
Richard Lee <ric@dicci.co.uk>
2018-08-15 17:38:33 +0100

    App service created in ARM template and attached to shared ASP

--- azure/ConvertTo-VSTSVariables.ps1
+++ azure/ConvertTo-VSTSVariables.ps1
@@ -8,7 +8,9 @@ $jsonvars = $ARMOutput | convertfrom-json
 # VSTS variables to be extracted from JSON
 $storageAccountName = $jsonvars.storageAccountName.value
 $storageConnectionString = $json.storageConnectionString.value
+$appName = $jsonvars.appServiceName.value
 
 # Write these out as VSTS variables
 Write-Host "##vso[task.setvariable variable=storageAccountName]$storageAccountName"
 Write-Host "##vso[task.setvariable variable=storageConnectionString]$storageConnectionString"
+Write-Host "##vso[task.setvariable variable=appName]$appName"
