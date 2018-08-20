6345ebcbc7b26e391aee6825481f91702e27f0fd
Mahmood Akhtar <mahmood233@hotmail.com>
2018-08-20 07:09:17 +0100

    DFC-4147 Find a Course View / DFC-4148 UI Validation - Using the gov toolkit / template I have started to layout the "Find a couse" page and add some simple client side validation.

--- src/Dfc.FindACourse.Web/ViewModels/CourseDirectory/IndexViewModel.cs
+++ src/Dfc.FindACourse.Web/ViewModels/CourseDirectory/IndexViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Dfc.FindACourse.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +10,9 @@ namespace Dfc.FindACourse.Web.ViewModels.CourseDirectory
     public class IndexViewModel
     {
         public RadiusDistance DefaultRadiusDistance => RadiusDistance.Miles10;
+
+        [Display(Name = "Course name")]
+        [Required]
+        public string SubjectKeyword { get; set; }
     }
 }
