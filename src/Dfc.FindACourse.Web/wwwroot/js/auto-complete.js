b157054047c92a7fa23920ca010f7da014dc2cfd
Mahmood Akhtar <mahmood233@hotmail.com>
2018-08-30 12:10:23 +0100

    DFC-4214 Cannot use keyboard to scroll or select from the autopopulate list - I have added arrow up/down key navigation through the suggestions list. Pressing the enter key selects an item.

--- src/Dfc.FindACourse.Web/wwwroot/js/auto-complete.js
+++ src/Dfc.FindACourse.Web/wwwroot/js/auto-complete.js
@@ -23,7 +23,7 @@
                         var termReplace = '<strong>' + term.toUpperCase() + '</strong>';
                         var formatted = '';
 
-                        if (valueLow.indexOf(term) > -1) {
+                        if (valueLow.indexOf(term.toLowerCase()) > -1) {
                             formatted = valueHigh.replace(term.toUpperCase(), termReplace);
                         } else {
                             formatted = valueHigh;
@@ -53,6 +53,68 @@
             }); 
         };
 
+        var arrowNagivation = function (keyCode) {
+            console.log(keyCode);
+            var currentItem = $('#course-list li.item-hover');
+
+            if (keyCode === 38) {
+                if (currentItem.length) {
+                    currentItem.removeClass('item-hover');
+
+                    if (currentItem.prev()) {
+                        currentItem.prev().addClass('item-hover');
+                    }
+
+                    scrollSuggestions('up');
+                }
+            }
+
+            if (keyCode === 40) {
+                if (currentItem.length) {
+                    currentItem.removeClass('item-hover');
+
+                    if (currentItem.next()) {
+                        currentItem.next().addClass('item-hover');
+                    }
+
+                    scrollSuggestions('down');
+                } else {
+                    $('#course-list li').first().addClass('item-hover');
+                    scrollSuggestions('down');
+                }
+            }
+
+        };
+
+        var scrollSuggestions = function (direction) {
+            var scrollableList = $('#course-list');
+            var currentItem = scrollableList.find('li.item-hover');
+            var height = scrollableList.innerHeight();
+            var currentVerticalScrollPos = scrollableList.scrollTop();
+            var itemHeight = currentItem.outerHeight(true);
+            var itemNumber = currentItem.index() + 1;
+
+            if (direction === 'down') {
+                if ((itemNumber * itemHeight) > height) {
+                    scrollableList.scrollTop(currentVerticalScrollPos + itemHeight);
+                }
+
+                if (!itemHeight) {
+                    scrollableList.scrollTop(0);
+                }
+            }
+
+            if (direction === 'up') {
+                scrollableList.scrollTop(currentVerticalScrollPos - itemHeight);
+
+                if (!itemHeight) {
+                    scrollableList.scrollTop(scrollableList[0].scrollHeight);
+                    currentItem.removeClass('item-hover');
+                    scrollableList.find('li:last-child').addClass('item-hover');
+                }
+            }
+        };
+
         $('#SubjectKeyword').on('keyup', function (e) {
             var keyCode = e.keyCode || e.which;
 
@@ -63,10 +125,20 @@
 
                 autoCompleteSuggestions($(this).val());
             }
+
+            if (keyCode === 38 || keyCode === 40) {
+                arrowNagivation(keyCode);
+            }
         });
 
         $('#SubjectKeyword').on('keydown', function (e) {
             var keyCode = e.keyCode || e.which;
+
+            if (keyCode === 13) {
+                e.preventDefault();
+                $('#SubjectKeyword').text($('#course-list li.item-hover').text());
+                $('#course-list').hide();
+            }
 
             if (keyCode === 9) {
                 $('#course-list').hide();
@@ -77,9 +149,14 @@
             $('#course-list').hide();
         });
 
-        $('#course-list').on('mouseleave', function () {
-            $(this).hide();
-            $('#SubjectKeyword').focus();
+        var timer;
+
+        $('.select-editable').on('mouseover', function () {
+            clearTimeout(timer);
+        }).on('mouseleave', function () {
+            timer = setTimeout(function () {
+                $('#course-list').hide();
+            }, 1500);
         });
 
         $('#course-list').on('click', function () {
