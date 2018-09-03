fa2032cf0a9393d7b8c7f743f4639b564d2cb10e
Karl Wootten <karl.wootten@obsequiosoftware.com>
2018-09-03 13:39:43 +0100

    Config updates and re-organisation

---
+++ src/Dfc.FindACourse.Common/Enums/AttendanceMode.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dfc.FindACourse.Common
+{
+    public enum AttendanceMode
+    {
+        [Display(Name = "Not Known")]
+        NotKnown = 0,
+        [Display(Name = "Location Campus")]
+        LocationCampus = 1,
+        [Display(Name = "Face To Face")]
+        FaceToFace = 2,
+        [Display(Name = "Work Based")]
+        WorkBased = 3,
+        [Display(Name = "Mixed Mode")]
+        MixedMode = 4,
+        [Display(Name = "Distance With Attendance")]
+        DistanceWithAttendance = 5,
+        [Display(Name = "Distance Without Attendence")]
+        DistanceWithoutAttendence = 6,
+        [Display(Name = "Online With Attendance")]
+        OnlineWithAttendance = 7,
+        [Display(Name = "Online Without Attendence")]
+        OnlineWithoutAttendence = 8
+    }
+}
