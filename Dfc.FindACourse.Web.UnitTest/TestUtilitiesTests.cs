e4326ca0ee696a89ccb8c1813d3658a289105d75
Andrew Bailey <andy@tbmj.co.uk>
2018-09-11 10:24:11 +0100

    Updated Coverage

---
+++ Dfc.FindACourse.Web.UnitTest/TestUtilitiesTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Dfc.FindACourse.TestUtilities.TestUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dfc.FindACourse.Web.UnitTest
+{
+    [TestClass]
+    public class TestUtilitiesTests
+    {
+        [TestMethod]
+        public void TestExceptionAsExpected()
+        {
+            var action = new Action(DoWorkWithException);
+            ComparisonsExtensions.Exception<Exception>(action, "expected");
+        }
+
+
+
+
+        public static void DoWorkWithException()
+        {
+            throw new Exception("expected");
+        }
+    }
+}
