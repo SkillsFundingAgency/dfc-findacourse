e9c94ba096105fcf843f9f5482a544a9fc26ac33
Mahmood Akhtar <mahmood233@hotmail.com>
2018-09-19 12:14:33 +0100

    Multiple choice behaviour for filters pn page 2. Client -side only nothing being sent back to server-side yet.

--- src/Dfc.FindACourse.Web/wwwroot/js/filters.js
+++ src/Dfc.FindACourse.Web/wwwroot/js/filters.js
@@ -18,5 +18,22 @@
         $('#QualificationLevelFilter').accordion();
 
         $('#Dfe1619FundedFilter').accordion();
+
+        $('.form-group .multiple-choice .multiple-choice-all').change(function () {
+            var itemCheckboxes = $(this).closest('.form-group').find('.multiple-choice .multiple-choice-item');
+            itemCheckboxes.prop('checked', $(this).is(':checked'));
+        });
+
+        $('.form-group .multiple-choice .multiple-choice-item').change(function () {
+            var allCheckboxes = $(this).closest('.form-group').find('.multiple-choice .multiple-choice-all');
+            var allItemCheckboxes = $(this).closest('.form-group').find('.multiple-choice .multiple-choice-item');
+            var checkedItemCheckboxes = $(this).closest('.form-group').find('.multiple-choice .multiple-choice-item:checked');
+
+            if (checkedItemCheckboxes.length === allItemCheckboxes.length) {
+                allCheckboxes.prop('checked', $(this).is(':checked'));
+            } else {
+                allCheckboxes.prop('checked', '');
+            }
+        });
     });
 })();
