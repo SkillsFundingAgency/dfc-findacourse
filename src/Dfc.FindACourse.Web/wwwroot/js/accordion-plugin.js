d9821f6080018e5602e6ff43e08438d6ab65cb47
Mahmood Akhtar <mahmood233@hotmail.com>
2018-09-26 12:45:05 +0100

    Accordion open and close behaviour on page 2. Now we maintain client-side stae for opened/closed accordions.

--- src/Dfc.FindACourse.Web/wwwroot/js/accordion-plugin.js
+++ src/Dfc.FindACourse.Web/wwwroot/js/accordion-plugin.js
@@ -8,7 +8,7 @@
     var methods = {
         init: function (options) {
             var settings = $.extend({
-                startState: 'close',
+                startState: 'close', // 'open' | 'close'
                 openStateClass: 'accordion--state-open',
                 closeStateClass: 'accordion--state-close',
                 speed: 'slow',
@@ -21,29 +21,31 @@
                 var accordion = $(this);
                 var header = accordion.children('.accordion-header');
                 var panel = header.next('.accordion-panel');
+                accordion.removeClass([settings.openStateClass, settings.closeStateClass]);
 
-                if (settings.startState === 'open') {
-                    accordion.addClass(settings.openStateClass);
-                    panel.show();
-                }
-
-                if (settings.startState === 'close') {
-                    accordion.addClass(settings.closeStateClass);
-                    panel.hide();
+                if (methods.getIsPostBack(accordion[0])) {
+                    if (settings.startState === 'open') {
+                        accordion.addClass(settings.openStateClass);
+                    } else if (settings.startState === 'close') {
+                        accordion.addClass(settings.closeStateClass);
+                    }
+                } else {
+                    var stateCssClass = methods.getStateCssClass(accordion[0], settings);
+                    accordion.addClass(stateCssClass);
                 }
 
                 header.on('click', function () {
                     panel.slideToggle(settings.speed, function () {
-                        if (accordion.hasClass(settings.openStateClass)) {
-                            accordion.removeClass(settings.openStateClass);
-                            accordion.addClass(settings.closeStateClass);
-                            if ($.isFunction(settings.close)) {
-                                settings.close.call(this, accordion[0]);
-                            }
-                        } else if (accordion.hasClass(settings.closeStateClass)) {
-                            accordion.removeClass(settings.closeStateClass);
-                            accordion.addClass(settings.openStateClass);
+                        var toggleState = methods.getToggleState(accordion[0], settings);
+                        accordion.removeClass(toggleState.removeCssClass);
+                        accordion.addClass(toggleState.addCssClass);
+
+                        if (toggleState.currentState === 'open') {
                             if ($.isFunction(settings.open)) {
+                                settings.open.call(this, accordion[0]);
+                            }
+                        } else if (toggleState.currentState === 'close') {
+                            if ($.isFunction(settings.close)) {
                                 settings.open.call(this, accordion[0]);
                             }
                         }
@@ -55,8 +57,59 @@
                 });
             });
         },
-        testy: function () {
-            console.log('testy called!!!');
+        getIsPostBack: function (el) {
+            var attrId = $(el).attr('id');
+            var val = window.sessionStorage.getItem(attrId + '::isPostBack') || null;
+
+            if (val === null) {
+                val = true;
+            } else {
+                val = false;
+            }
+
+            methods.setIsPostBack(el, val);
+
+            return val;
+        },
+        setIsPostBack: function (el, val) {
+            var attrId = $(el).attr('id');
+            window.sessionStorage.setItem(attrId + '::isPostBack', val);
+        },
+        getStateCssClass: function (el, settings) {
+            var attrId = $(el).attr('id');
+            var val = window.sessionStorage.getItem(attrId + '::stateCssClass') || null;
+
+            if (val === null) {
+
+                if (settings.startState === 'open') {
+                    val = settings.openStateClass;
+                } else if (settings.startState === 'close') {
+                    val = settings.closeStateClass;
+                }
+
+                methods.setStateCssClass(el, val);
+            }
+
+            console.log([attrId, 'getStateCssClass', val]);
+
+            return val;
+        },
+        setStateCssClass: function (el, val) {
+           var attrId = $(el).attr('id');
+            window.sessionStorage.setItem(attrId + '::stateCssClass', val);
+
+            console.log([attrId, 'setStateCssClass', val]);
+        },
+        getToggleState: function(el, settings) {
+            var current = methods.getStateCssClass(el, settings);
+
+            if (current === settings.openStateClass) {
+                methods.setStateCssClass(el, settings.closeStateClass);
+                return { currentState: 'close', previousState: 'open', removeCssClass: settings.openStateClass, addCssClass: settings.closeStateClass };
+            } else {
+                methods.setStateCssClass(el, settings.openStateClass);
+                return { currentState: 'open', previousState: 'close', removeCssClass: settings.closeStateClass, addCssClass: settings.openStateClass };
+            }
         }
     };
 
