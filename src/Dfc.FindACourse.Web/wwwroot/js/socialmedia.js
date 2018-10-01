cfed38ab693e23cc421921949121b69f83cccf11
Karl Wootten <karl.wootten@obsequiosoftware.com>
2018-10-01 14:20:43 +0100

    DFC-4641, DFC-4589

--- src/Dfc.FindACourse.Web/wwwroot/js/socialmedia.js
+++ src/Dfc.FindACourse.Web/wwwroot/js/socialmedia.js
@@ -8,6 +8,6 @@
         $('.twitter').attr('href', 'https://twitter.com/intent/tweet?original_referer&amp;url=' + window.location.href);
         $('.facebook').attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.href);
         $('.linkedin').attr('href', 'https://www.linkedin.com/shareArticle?url=' +window.location.href);
-        $('.email').attr('href', 'mailto:?subject=I wanted to share this post with you from the National Careers Service: ' + window.location.href);
+        $('.email').attr('href', 'mailto:?subject=National Careers Service&body=I wanted to share this post with you from the National Careers Service:%0D%0A' + window.location.href);
     });
 })();
