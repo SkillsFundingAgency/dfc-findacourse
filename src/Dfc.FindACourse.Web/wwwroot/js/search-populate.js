037e5e3bd04232fde5f32c514c680751b10ffdb0
Karl Wootten <karl.wootten@obsequiosoftware.com>
2018-10-01 11:18:53 +0100

    DFC-4628/DFC-4629 Reversed changes nade last week that were persisting wrong search state

--- src/Dfc.FindACourse.Web/wwwroot/js/search-populate.js
+++ src/Dfc.FindACourse.Web/wwwroot/js/search-populate.js
@@ -9,7 +9,7 @@
             name = name.replace(/[[]/, "\\[").replace(/[\]]/, "\\]");
             var regexS = "[\\?&]" + name + "=([^&#]*)";
             var regex = new RegExp(regexS);
-            var results = regex.exec(document.referrer);
+            var results = regex.exec($('#hidUrl').val());
             if (results == null)
                 return "";
             else
@@ -17,14 +17,23 @@
         }
         //get vars
         var subject = GetURLParam('SubjectKeyword');
-        var qualLevels = GetURLParam('QualificationLevels');
-        var radius = GetURLParam('LocationRadius');
-        var postcode = GetURLParam('Location');
+        //var qualLevels = GetURLParam('QualificationLevels');
+        //var radius = GetURLParam('LocationRadius');
+        //var postcode = GetURLParam('Location');
 
-        //set inputs
-        $('#QualificationLevels').val(qualLevels);
-        $('#LocationRadius').val(radius);
-        if (subject.length > 0) $('#SubjectKeyword').val(decodeURIComponent(subject).replace(/\+/g, ' '));
-        if (postcode.length > 0) $('#Location').val(decodeURIComponent(postcode).replace(/\+/g, ' '));
+        ////set inputs
+        //$('#QualificationLevels').val(qualLevels);
+        //$('#LocationRadius').val(radius);         if (subject.length > 0) $('#SubjectKeyword').val(decodeURIComponent(subject).replace(/\+/g, ' '));
+        //if (postcode.length > 0) $('#Location').val(decodeURIComponent(postcode).replace(/\+/g, ' '));
+
+        //pure javascript
+        //var pathname = window.location.pathname;
+        //alert('pathname' + pathname);
+        //// to show it in an alert window
+        //alert(window.location);
+        if (window.location.href.indexOf("?") > -1) {
+           /// alert("your url contains the name franky");
+            $('#hidUrl').val(window.location.href);
+        }
     });
 })();
