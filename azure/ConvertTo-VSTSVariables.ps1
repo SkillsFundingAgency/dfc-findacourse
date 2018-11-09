d3f02a1b5c09d75c9488b975c76494831af6704d
Richard Lee <ric@dicci.co.uk>
2018-11-09 17:05:33 +0000

    Securestrings in ARM template outputs

--- azure/ConvertTo-VSTSVariables.ps1
+++ azure/ConvertTo-VSTSVariables.ps1
@@ -4,18 +4,19 @@ param (
 
 # Output from ARM template is a JSON document
 $jsonvars = $ARMOutput | convertfrom-json
-Write-Host $ARMOutput
 
-# VSTS variables to be extracted from JSON
-$storageAccountName = $jsonvars.storageAccountName.value
-$storageConnectionString = $json.storageConnectionString.value
-$appName = $jsonvars.appServiceName.value
-$appInsights = $jsonvars.appInsightInstrumentationKey.value
-$appInsightAppId = $jsonvars.appInsightAppId.value
+# the outputs with be of type noteproperty, get a list of all of them
+foreach ($outputname in ($jsonvars | Get-Member -MemberType NoteProperty).name) {
+    # get the type and value for each output
+    $outtypevalue = $jsonvars | Select-Object -ExpandProperty $outputname
+    $outtype = $outtypevalue.type
+    $outvalue = $outtypevalue.value
 
-# Write these out as VSTS variables
-Write-Host "##vso[task.setvariable variable=storageAccountName]$storageAccountName"
-Write-Host "##vso[task.setvariable variable=storageConnectionString]$storageConnectionString"
-Write-Host "##vso[task.setvariable variable=appName]$appName"
-Write-Host "##vso[task.setvariable variable=appInsights]$appInsights"
-Write-Host "##vso[task.setvariable variable=appInsightAppId]$appInsightAppId"
+    # Set VSTS variable
+    if ($outtype.toLower() -eq 'securestring') {
+        Write-Host "##vso[task.setvariable variable=$outputname;issecret=true]$outvalue"
+    }
+    else {
+        Write-Host "##vso[task.setvariable variable=$outputname]$outvalue"
+    }
+}
